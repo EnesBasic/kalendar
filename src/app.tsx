1bed14048ab687b8e7c7a73695bf28d8b90c87ac
EnesBasic <ssamss3004@gmail.com>
2025-06-26 05:18:05 +0200

    init

--- src/app.tsx
+++ src/app.tsx
@@ -5,9 +5,7 @@ function App() {
     <div>
       <h1>Welcome to Kalendar!</h1>
       <p>To get started, edit <code>src/App.tsx</code> and save to reload.</p>
-      <p>Run <code>npm install</code> to install dependencies.</p>
-      <p>Run <code>npm start</code> to start the development server.</p>
-      <p>cd ~/kalendar/BINBIN</p>
+    
     </div>
   );
 }
